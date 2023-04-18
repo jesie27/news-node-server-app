c6841ce4709eabd8791f53d90b45b6bf330bb01b
Jesie F <jesiefu11@gmail.com>
2023-04-17 23:23:12 -0400

    proj edits

--- News/controllers/users/users-controller.js
+++ News/controllers/users/users-controller.js
@@ -1,11 +1,17 @@
 import people from './users.js'
 let users = people
+
+let currentUser = null;
 const UserController = (app) => {
     app.get('/api/users', findUsers);
     app.get('/api/users/:uid', findUserById);
     app.post('/api/users', createUser);
     app.delete('/api/users/:uid', deleteUser);
     app.put('/api/users/:uid', updateUser);
+    app.post('api/login', login);
+    app.post('api/logout', logout);
+    app.get('api/profile', profile);
+    app.post('api/register', register);
 
 }
 
@@ -49,7 +55,47 @@ const updateUser = (req, res) => {
     res.sendStatus(200);
 }
 
+const login = (req, res) => {
+    const user = req.body;
+    const foundUser = users.find(
+        (user) => user.username === req.body.username &&
+            user.password === req.body.password
+    );
+    if (foundUser) {
+        currentUser = foundUser;
+        res.send(foundUser);
+    }
+    else {
+        res.sendStatus(404);
+    }
+};
+const logout = (req, res) => {
+    currentUser = null;
+    res.sendStatus(204);
+};
 
+const profile = (req, res) => {
+    if (currentUser) {
+        res.send(currentUser);
+    }
+    else {
+        res.sendStatus(404);
+    }
+};
+
+const register = (req, res) => {
+    const user = req.body;
+    const foundUser = users.find((user) => user.username === req.body.username)
+    if (foundUser) {
+        res.sendStatus(404);
+    }
+    else {
+        const newUser = {...user, id: new Date().getTime()};
+        currentUser = newUser;
+        users.push(newUser);
+        res.sendStatus(201);
+    }
+}
 
 
 export default UserController
---
+++ services/users-service.js
@@ -0,0 +1,2 @@
+import axios from "axios";
+const USERS_API_URL = "http://localhost:4000/api/users"
